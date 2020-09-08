8574dfab72c7cc7c980ad6f27e4fe4cb72d09949
Radomir Dinic <radomir.dinic@fh-salzburg.ac.at>
2020-09-08 14:57:42 +0200

    not working with my board

--- config/examples/Creality/Ender-3 Pro/Configuration_adv.h
+++ config/examples/Creality/Ender-3 Pro/Configuration_adv.h
@@ -1620,7 +1620,7 @@
  *
  * See https://marlinfw.org/docs/features/lin_advance.html for full instructions.
  */
-#define LIN_ADVANCE
+//#define LIN_ADVANCE
 #if ENABLED(LIN_ADVANCE)
   //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
   #define LIN_ADVANCE_K 0.6    // Unit: mm compression per 1mm/s extruder speed
@@ -1761,7 +1761,7 @@
 //
 // G2/G3 Arc Support
 //
-//#define ARC_SUPPORT                 // Disable this feature to save ~3226 bytes
+#define ARC_SUPPORT                 // Disable this feature to save ~3226 bytes
 #if ENABLED(ARC_SUPPORT)
   #define MM_PER_ARC_SEGMENT      1 // (mm) Length (or minimum length) of each arc segment
   //#define ARC_SEGMENTS_PER_R    1 // Max segment length, MM_PER = Min
