f04c7dd6a50fc013a5bac667daf0b12c9f1df40d
Radomir Dinic <radomir.dinic@fh-salzburg.ac.at>
2020-10-09 02:31:49 +0200

    update z offset and test action commands

--- config/examples/Creality/Ender-3/BigTreeTech SKR Mini E3 2.0/Configuration.h
+++ config/examples/Creality/Ender-3/BigTreeTech SKR Mini E3 2.0/Configuration.h
@@ -980,7 +980,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -42, -22, -1.975 }
+#define NOZZLE_TO_PROBE_OFFSET { -42, -22, -2 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
